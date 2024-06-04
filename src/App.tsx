00e285b470e06b951b0752941ad7ad67b0122ca6
Gallie83 <keving2913@hotmail.com>
2024-06-04 14:19:13 -0700

    Added React Big Calendar library with basic event interface

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// Configure Moment.js as the localizer
+const localizer = momentLocalizer(moment);
+
+interface MyEvent {
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  startTime: moment.Moment;
+  endTime: moment.Moment;
+}
+
+interface MyCalendarProps {
+  events: MyEvent[];
+}
+
+const MyCalendar: React.FC<MyCalendarProps> = ({ events }) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="startDate"
+      endAccessor="endDate"
+      style={{ height: 1000, width: 1000 }}
+    />
+  </div>
+);
+
+export default MyCalendar;
