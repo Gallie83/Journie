e4d8efffcabe1483a4cd8e7d4656b022f40ddd71
Gallie83 <keving2913@hotmail.com>
2024-07-05 11:02:07 -0700

    Created postgres DB and generated prisma client

---
+++ server/prisma/migrations/20240705174805_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "JournalEntry" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "images" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "JournalEntry_pkey" PRIMARY KEY ("id")
+);
